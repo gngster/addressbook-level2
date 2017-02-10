ae9bf3365cd4e2897a22a15cf8b93b75fe1975b2
gngster <gjhsmiles@gmail.com>
2017-02-10 22:43:34 +0800

    added getalltags method

--- src/seedu/addressbook/data/tag/Tagging.java
+++ src/seedu/addressbook/data/tag/Tagging.java
@@ -3,22 +3,39 @@ package seedu.addressbook.data.tag;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import seedu.addressbook.data.person.UniquePersonList;
+import seedu.addressbook.data.person.Person;
 
 public class Tagging {
-	UniquePersonList persons;
-	HashMap <UniquePersonList, String> mapOfTags;
+	Person person;
+	HashMap <Person, String> mapOfTags;
 	static ArrayList <String> tagsDeleted;
 	static ArrayList <String> tagsAdded;
 	ArrayList<String> tagsToPerson;
 	
-	public Tagging(UniquePersonList person, String tag, boolean value) {
+	public Tagging(Person person, String tag, boolean value) {
 		if(value == true) {
-			tagsToPerson.add(tag);	
+			tagsToPerson.add(tag);
+			String stringToPrint = person.getName().toString() + "[" + tag + "]";
+			tagsAdded.add(stringToPrint);
 		}
+		
 		if(value == false) {
 			tagsToPerson.remove(tag);
+			String stringToPrint = person.getName().toString() + "[" + tag + "]";
+			tagsDeleted.add(stringToPrint);
 		}	
 	}
+	
+	public String getAllTagsModified() {
+		String stringOfTags = "";
+		for(String ta: tagsAdded) {
+			stringOfTags += "+" + ta + "/r/n";
+		}
+		for(String td: tagsDeleted) {
+			stringOfTags += "-" + td + "/r/n";
+ 		}
+		return stringOfTags;
+	}
+	
 	
 }
